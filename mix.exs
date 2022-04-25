58fa612de1c73dbdddb51326b4e676d2e33c0951
Pavel Makarenko <cryfall@gmail.com>
2022-04-25 11:57:14 +0400

    added description

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule ExEip712.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: "ExEip712",
+      source_url: "https://github.com/m1ome/ex_eip712"
     ]
   end
 
@@ -22,6 +26,22 @@ defmodule ExEip712.MixProject do
   defp deps do
     [
       {:rustler, "~> 0.23.0"}
+    ]
+  end
+
+  defp description() do
+    "Package for simplifying EIP712 signature creation from Elixir"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "ex_eip712",
+      # These are the default files included in the package
+      files: ~w(lib native .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/m1ome/ex_eip712"}
     ]
   end
 end
